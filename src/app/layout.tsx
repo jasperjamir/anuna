c139d03529def3b37bd7e42994c70f9fef4b0b2c
Jasper Jamir <jasperjamir@gmail.com>
2025-09-13 17:59:17 +0800

    bug(build); fix build errors'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export const metadata: Metadata = {
     title: "Anuna",
     description: "Anuna?!",
     type: "website",
-    robots: "noindex, nofollow, noarchive, nosnippet, noimageindex",
   },
   twitter: {
     card: "summary",
